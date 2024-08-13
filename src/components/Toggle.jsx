c906464f2fcb52af225ea149c38967692f2cc755
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-13 20:03:58 +0200

    insert custom properties to svg and add aria to toggle.

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,10 +3,17 @@
 export default function Toggle() {
   return (
     <div className="primary-header__toggle-container flex">
-        <input className="toggle" id="theme-toggle" type="checkbox" aria-label="change page theme" />
+        <input 
+        className="toggle" 
+        id="theme-toggle" 
+        type="checkbox" 
+        role="switch"
+        aria-label="change page theme"
+        aria-checked="false"
+        />
 
         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22">
-            <path fill="none" stroke="#838383" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/>
+            <path fill="none" stroke="var(--icon-color, #838383)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/>
         </svg>
     </div>
   )
