4f87f42cca065608f0569ca44f33271f8079bece
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-08-15 12:15:33 +0200

    remove unused component.

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import ErrorPage from "./pages/ErrorPage";
 import DefinitionDetails from "./pages/DefinitionDetails";
 
 export const router = createBrowserRouter([
